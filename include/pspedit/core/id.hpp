f84d866a271bb2a702d949a0e81672bea1cc8bb6
adriensalon <adrien.salon@live.fr>
2026-02-24 14:13:40 +0100

    Simpler ids

--- include/pspedit/core/id.hpp
+++ include/pspedit/core/id.hpp
@@ -7,16 +7,15 @@ namespace pspedit {
 using entity_id = u32;
 
 struct object_id {
-    u64 low;
-    u64 high;
+    u32 value = 0;
 
     [[nodiscard]] inline bool has_value() const
     {
-        return (low != 0) && (high != 0);
+        return value != 0;
     }
 };
 
-static constexpr object_id null_object_id { 0, 0 };
+static constexpr object_id null_object_id { 0 };
 
 struct runtime_id {
     u64 low;
